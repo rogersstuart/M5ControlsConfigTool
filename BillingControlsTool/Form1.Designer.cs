596f724d02c6010ac34d4813d7c451ab6d15f44d
Stuart Rogers <rogersstuart@gmail.com>
2019-12-05 17:43:39 -0600

    Add project files.

---
+++ BillingControlsTool/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace BillingControlsTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(317, 272);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBox3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.textBox4, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox5, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 52);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(309, 168);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SSID";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "USD To BIll";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Load Active For";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(158, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox2.Location = new System.Drawing.Point(158, 80);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 24);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox3.Location = new System.Drawing.Point(158, 143);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 24);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox4.Location = new System.Drawing.Point(158, 115);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(147, 24);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "API Key";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox5.Location = new System.Drawing.Point(158, 6);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(147, 24);
+            this.textBox5.TabIndex = 9;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.AutoSize = true;
+            this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 228);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(309, 40);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(38, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Write";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.AutoSize = true;
+            this.tableLayoutPanel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel4.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(309, 40);
+            this.tableLayoutPanel4.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(4, 7);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Enter += new System.EventHandler(this.comboBox1_Enter);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button3.Location = new System.Drawing.Point(192, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 32);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 272);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Controls System Configuration";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BillingControlsTool/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BillingControlsTool
+{
+    public partial class Form1 : Form
+    {
+        struct WiFICredentials
+        {
+            public string ssid;
+            public string password;
+
+            public WiFICredentials(string ssid, string password)
+            {
+                this.ssid = ssid;
+                this.password = password;
+            }
+        }
+
+        struct OptionsExtension
+        {
+            public string api_key;
+            public string to_bill;
+            public int active_duration;
+
+            public OptionsExtension(string api_key, string to_bill, int active_duration)
+            {
+                this.api_key = api_key;
+                this.to_bill = to_bill;
+                this.active_duration = active_duration;
+            }
+        }
+
+        struct AppConfig
+        {
+            public WiFICredentials cred;
+            public OptionsExtension opex;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(File.Exists("config.json"))
+            {
+                var cfg = File.ReadAllText("config.json");
+
+                var lcfg = JsonConvert.DeserializeObject<AppConfig>(cfg, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+                textBox1.Text = lcfg.cred.ssid;
+                textBox2.Text = lcfg.cred.password;
+
+                textBox3.Text = lcfg.opex.active_duration > -1 ? (lcfg.opex.active_duration + "") : "";
+                textBox4.Text = lcfg.opex.to_bill;
+                textBox5.Text = lcfg.opex.api_key;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+                c.Enabled = false;
+
+            //read
+            string sel = (string)comboBox1.SelectedItem;
+
+            await Task.Run(async () =>
+            {
+                using (SerialPort com_connection = new SerialPort(sel, 115200, Parity.None, 8, StopBits.One))
+                {
+                    com_connection.NewLine = "\r\n";
+                    com_connection.ReadTimeout = 2000;
+                    com_connection.WriteTimeout = 2000;
+
+                    com_connection.Open();
+
+                    StreamWriter w = new StreamWriter(com_connection.BaseStream);
+                    StreamReader r = new StreamReader(com_connection.BaseStream);
+                    w.AutoFlush = true;
+
+                    //read wifi credentials
+
+                    w.Write('k'); //programming mode command
+
+                    //programming mode key
+                    w.Write('p');
+                    w.Write('r');
+                    w.Write('o');
+                    w.Write('g');
+
+                    while (r.Read() != 'z')//ack
+                        await Task.Delay(2000);
+
+                    char[] buffer = new char[200];
+
+                    w.Write('r'); //read credentials command
+
+                    r.ReadBlock(buffer, 0, 200);
+
+                    var dser = JsonConvert.DeserializeObject<WiFICredentials>(new string(buffer), new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+                    Invoke((MethodInvoker)(() =>
+                    {
+                        textBox1.Text = dser.ssid;
+                        textBox2.Text = dser.password;
+                    }));
+
+                    ////////////////////////////////////////////////////////////////////////////////
+
+                    //read extended params
+
+                    w.Write('k'); //programming mode command
+
+                    //programming mode key
+                    w.Write('p');
+                    w.Write('r');
+                    w.Write('o');
+                    w.Write('g');
+
+                    while (r.Read() != 'z') //ack
+                        await Task.Delay(2000);
+                    
+                    buffer = new char[200];
+
+                    w.Write('b'); //read extended params
+
+                    r.ReadBlock(buffer, 0, 400);
+
+                    var dser2 = JsonConvert.DeserializeObject<OptionsExtension>(new string(buffer), new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+                    Invoke((MethodInvoker)(() =>
+                    {
+                        textBox4.Text = dser2.to_bill;
+                        textBox3.Text = dser2.active_duration + "";
+                        textBox5.Text = dser2.api_key;
+                    }));
+
+                    //finished reading params
+
+                    com_connection.Close();
+
+                    Invoke((MethodInvoker)(() => {
+                        foreach (Control c in Controls)
+                            c.Enabled = true;
+                    }));
+                }
+
+                Console.WriteLine("done reading");
+            });
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            //write
+
+            foreach (Control c in Controls)
+                c.Enabled = false;
+
+            string sel = (string)comboBox1.SelectedItem;
+
+            using (SerialPort com_connection = new SerialPort(sel, 115200, Parity.None, 8, StopBits.One))
+            {
+                com_connection.NewLine = "\r\n";
+                com_connection.ReadTimeout = 2000;
+                com_connection.WriteTimeout = 2000;
+
+                com_connection.Open();
+
+                StreamWriter w = new StreamWriter(com_connection.BaseStream);
+                StreamReader r = new StreamReader(com_connection.BaseStream);
+                w.AutoFlush = true;
+
+                w.Write('k'); //programming mode command
+
+                //programming mode key
+                w.Write('p');
+                w.Write('r');
+                w.Write('o');
+                w.Write('g');
+
+                while (r.Read() != 'z') //ack
+                    await Task.Delay(2000);
+
+                char[] buffer = new char[200];
+
+                string wifi_cred = "{\"ssid\":\"" + textBox1.Text.Trim() + "\",\"password\":\"" + textBox2.Text.Trim() + "\"}";
+
+                var chars = ASCIIEncoding.ASCII.GetBytes(wifi_cred);
+                chars.CopyTo(buffer, 0);
+
+                w.Write('w');
+
+                w.Write(buffer);
+
+                r.Read();
+
+                /////////////////////////////////////////////////////////////////////
+                
+                w.Write('k'); //programming mode command
+
+                //programming mode key
+                w.Write('p');
+                w.Write('r');
+                w.Write('o');
+                w.Write('g');
+
+                while (r.Read() != 'z') //ack
+                    await Task.Delay(2000);
+
+                buffer = new char[200];
+
+                string ext_param = "{\"api_key\":\"" + textBox5.Text.Trim() + "\",\"to_bill\":\"" + textBox4.Text.Trim() + "\",\"active_duration\":\"" + textBox3.Text.Trim() + "\"}";
+
+                var chars2 = ASCIIEncoding.ASCII.GetBytes(wifi_cred);
+                chars.CopyTo(buffer, 0);
+
+                w.Write('s'); //write params key
+
+                w.Write(buffer);
+
+                r.Read();
+
+                //finished writing
+
+                com_connection.Close();
+
+                Invoke((MethodInvoker)(() => {
+                    foreach (Control c in Controls)
+                        c.Enabled = true;
+                }));
+            }
+
+            Console.WriteLine("done writing");
+        }
+
+        private void comboBox1_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_Enter(object sender, EventArgs e)
+        {
+            refresh_com_ports();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //form shown
+
+            refresh_com_ports();
+        }
+
+        private void refresh_com_ports()
+        {
+            var ports = SerialPort.GetPortNames();
+
+            comboBox1.DataSource = ports;
+
+            if (ports.Length > 0)
+                comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //form is closing. save the fields
+            AppConfig cfg;
+
+            int val;
+            if (!Int32.TryParse(textBox3.Text, out val))
+                val = -1;
+
+            cfg.opex = new OptionsExtension(textBox5.Text, textBox4.Text, val);
+            cfg.cred = new WiFICredentials(textBox1.Text, textBox2.Text);
+
+
+            File.WriteAllText("config.json",JsonConvert.SerializeObject(cfg));
+        }
+    }
+
+
+}
